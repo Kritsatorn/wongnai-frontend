a99754fd2ecf829b45828af6728258c2387b3499
Kritsatorn Saengweang <kritsatornsaengweang@MacBook-Pro-4.local>
2021-02-20 18:51:29 +0700

    del

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
 function App() {
-  return <div className="text-blue-500 bg-blue-200 ">Hello WOrld</div>;
+  return <div className="text-blue-500 bg-blue-200">Hello WOrld</div>;
 }
 export default App;
